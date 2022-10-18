8fe7b79a617b786144039c8c0febd9496b21feda
peanattha <nuttapong.tho@kkumail.com>
2022-10-19 00:19:31 +0700

    first commit

--- resources/views/profile/update-profile-information-form.blade.php
+++ resources/views/profile/update-profile-information-form.blade.php
@@ -85,7 +85,7 @@
             {{ __('Saved.') }}
         </x-jet-action-message>
 
-        <x-jet-button wire:loading.attr="disabled" wire:target="photo">
+        <x-jet-button wire:loading.attr="disabled" wire:target="photo" id="save">
             {{ __('Save') }}
         </x-jet-button>
     </x-slot>
